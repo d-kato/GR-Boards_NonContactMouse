8252077e02935fc6715933b8593c9732ee8a3803
d-kato <daiki.kato.cj@renesas.com>
2018-04-20 11:29:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# GR-Boards_NonContactMouse
+GR-PEACH、および、GR-LYCHEEで動作するサンプルプログラムです。  
+GR-LYCHEEの開発環境については、[GR-LYCHEE用オフライン開発環境の手順](https://developer.mbed.org/users/dkato/notebook/offline-development-lychee-langja/)を参照ください。
+
+
+## 概要
+カメラを使った非接触USBマウスのサンプルです。OpenCVの optical flow を使い、カメラに写った物体の動きをベクトルで表します。各ベクトルの平均値から、マウスポインタの移動距離を決定します。
+
+カメラの指定を行う場合(GR-PEACHのみ)は``mbed_app.json``に``camera-type``を追加してください。
+```json
+{
+    "config": {
+        "camera":{
+            "help": "0:disable 1:enable",
+            "value": "1"
+        },
+        "camera-type":{
+            "help": "Please see mbed-gr-libs/README.md",
+            "value": "CAMERA_CVBS"
+        },
+        "lcd":{
+            "help": "0:disable 1:enable",
+            "value": "0"
+        }
+    }
+}
+```
+
+| camera-type "value"     | 説明                               |
+|:------------------------|:-----------------------------------|
+| CAMERA_CVBS             | GR-PEACH NTSC信号                  |
+| CAMERA_MT9V111          | GR-PEACH MT9V111                   |
+| CAMERA_OV7725           | GR-LYHCEE 付属カメラ               |
+| CAMERA_OV5642           | GR-PEACH OV5642                    |
+| CAMERA_WIRELESS_CAMERA  | GR-PEACH Wireless/Cameraシールド (OV7725) |
+
+camera-typeを指定しない場合は以下の設定となります。  
+* GR-PEACH、カメラ：CAMERA_MT9V111  
+* GR-LYCHEE、カメラ：CAMERA_OV7725  
+
+***Mbed CLI以外の環境で使用する場合***  
+Mbed以外の環境(CLI or Mbedオンラインコンパイラ 以外の環境)をお使いの場合、``mbed_app.json``の変更は反映されません。  
+``mbed_config.h``に以下のようにマクロを追加してください。  
+```cpp
+#define MBED_CONF_APP_CAMERA                        1    // set by application
+#define MBED_CONF_APP_CAMERA_TYPE                   CAMERA_CVBS             // set by application
+#define MBED_CONF_APP_LCD                           0    // set by application
+```
---
+++ main.cpp
@@ -0,0 +1,160 @@
+#include "mbed.h"
+#include "opencv.hpp"
+#include "EasyAttach_CameraAndLCD.h"
+#include "USBMouse.h"
+
+#define PLOT_INTERVAL          (30)
+#define DIST_SCALE_FACTOR_X    (6.0)
+#define DIST_SCALE_FACTOR_Y    (6.0)
+
+/*! Frame buffer stride: Frame buffer stride should be set to a multiple of 32 or 128
+    in accordance with the frame buffer burst transfer mode. */
+#define VIDEO_PIXEL_HW         (160u)  /* HQVGA */
+#define VIDEO_PIXEL_VW         (120u)  /* HQVGA */
+
+#define FRAME_BUFFER_STRIDE    (((VIDEO_PIXEL_HW * 2) + 31u) & ~31u)
+#define FRAME_BUFFER_HEIGHT    (VIDEO_PIXEL_VW)
+
+#if defined(__ICCARM__)
+#pragma data_alignment=32
+static uint8_t user_frame_buffer0[FRAME_BUFFER_STRIDE * FRAME_BUFFER_HEIGHT]@ ".mirrorram";
+#else
+static uint8_t user_frame_buffer0[FRAME_BUFFER_STRIDE * FRAME_BUFFER_HEIGHT]__attribute((section("NC_BSS"),aligned(32)));
+#endif
+static volatile int Vfield_Int_Cnt = 0;
+
+DisplayBase Display;
+DigitalOut  led1(LED1);
+USBMouse    mouse;
+
+static void IntCallbackFunc_Vfield(DisplayBase::int_type_t int_type) {
+    if (Vfield_Int_Cnt > 0) {
+        Vfield_Int_Cnt--;
+    }
+}
+
+static void wait_new_image(void) {
+    Vfield_Int_Cnt = 1;
+    while (Vfield_Int_Cnt > 0) {
+        Thread::wait(1);
+    }
+}
+
+static void Start_Video_Camera(void) {
+    // Field end signal for recording function in scaler 0
+    Display.Graphics_Irq_Handler_Set(DisplayBase::INT_TYPE_S0_VFIELD, 0, IntCallbackFunc_Vfield);
+
+    // Video capture setting (progressive form fixed)
+    Display.Video_Write_Setting(
+        DisplayBase::VIDEO_INPUT_CHANNEL_0,
+        DisplayBase::COL_SYS_NTSC_358,
+        (void *)user_frame_buffer0,
+        FRAME_BUFFER_STRIDE,
+        DisplayBase::VIDEO_FORMAT_YCBCR422,
+        DisplayBase::WR_RD_WRSWA_32_16BIT,
+        VIDEO_PIXEL_VW,
+        VIDEO_PIXEL_HW
+    );
+    EasyAttach_CameraStart(Display, DisplayBase::VIDEO_INPUT_CHANNEL_0);
+}
+
+#if MBED_CONF_APP_LCD
+static void Start_LCD_Display(void) {
+    DisplayBase::rect_t rect;
+
+    rect.vs = (LCD_PIXEL_HEIGHT - VIDEO_PIXEL_VW) / 2;
+    rect.vw = VIDEO_PIXEL_VW;
+    rect.hs = (LCD_PIXEL_WIDTH - VIDEO_PIXEL_HW);
+    rect.hw = VIDEO_PIXEL_HW;
+    Display.Graphics_Read_Setting(
+        DisplayBase::GRAPHICS_LAYER_0,
+        (void *)user_frame_buffer0,
+        FRAME_BUFFER_STRIDE,
+        DisplayBase::GRAPHICS_FORMAT_YCBCR422,
+        DisplayBase::WR_RD_WRSWA_32_16BIT,
+        &rect
+    );
+    Display.Graphics_Start(DisplayBase::GRAPHICS_LAYER_0);
+
+    Thread::wait(50);
+    EasyAttach_LcdBacklight(true);
+}
+#endif
+
+int main() {
+    cv::Mat prev_image;
+    cv::Mat curr_image;
+    std::vector<cv::Point2f> prev_pts;
+    std::vector<cv::Point2f> curr_pts;
+    cv::Point2f point;
+    int16_t  x = 0;
+    int16_t  y = 0;
+
+    EasyAttach_Init(Display);
+    Start_Video_Camera();
+#if MBED_CONF_APP_LCD
+    Start_LCD_Display();
+#endif
+
+    // Initialization of optical flow
+    point.y = (VIDEO_PIXEL_VW / 2) + (PLOT_INTERVAL * 1);
+    for (int32_t i = 0; i < 3; i++) {
+        point.x = (VIDEO_PIXEL_HW / 2) - (PLOT_INTERVAL * 1);
+        for (int32_t j = 0; j < 3; j++) {
+            prev_pts.push_back(point);
+            point.x += PLOT_INTERVAL;
+        }
+        point.y -= PLOT_INTERVAL;
+    }
+
+    while (1) {
+        // Wait for image input
+        wait_new_image();
+
+        // Convert from YUV422 to grayscale
+        cv::Mat img_yuv(VIDEO_PIXEL_VW, VIDEO_PIXEL_HW, CV_8UC2, user_frame_buffer0);
+        cv::cvtColor(img_yuv, curr_image, cv::COLOR_YUV2GRAY_YUY2);
+
+        point = cv::Point2f(0, 0);
+        if ((!curr_image.empty()) && (!prev_image.empty())) {
+            // Optical flow
+            std::vector<uchar> status;
+            std::vector<float> err;
+            cv::calcOpticalFlowPyrLK(prev_image, curr_image, prev_pts, curr_pts, status, err, cv::Size(21, 21), 0);
+
+            // Setting movement distance of feature point
+            std::vector<cv::Scalar> samples;
+            for (size_t i = 0; i < (size_t)status.size(); i++) {
+                if (status[i]) {
+                    cv::Point2f vec = curr_pts[i] - prev_pts[i];
+                    cv::Scalar sample = cv::Scalar(vec.x, vec.y);
+                    samples.push_back(sample);
+                }
+            }
+
+            // Mean and standard deviation
+            if (samples.size() >= 6) {
+                cv::Scalar mean;
+                cv::Scalar stddev;
+                cv::meanStdDev((cv::InputArray)samples, mean, stddev);
+                //printf("%d,  stddev=%lf, %lf\r\n", samples.size(), stddev[0], stddev[1]);  // for debug
+                if ((stddev[0] < 10.0) && (stddev[1] < 10.0)) {
+                    point.x = mean[0];
+                    point.y = mean[1];
+                }
+            }
+        }
+        cv::swap(prev_image, curr_image);
+
+        x = (int16_t)(point.x * DIST_SCALE_FACTOR_X) * -1;
+        y = (int16_t)(point.y * DIST_SCALE_FACTOR_Y);
+
+        if ((x != 0) || (y != 0)) {
+            led1 = 1;
+            //printf("x=%d, y=%d\r\n", x, y);  // for debug
+            mouse.move(x, y);
+        } else {
+            led1 = 0;
+        }
+    }
+}
